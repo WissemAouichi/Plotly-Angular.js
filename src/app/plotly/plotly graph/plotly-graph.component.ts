9dc42a1decca1a8252cff5aec78ce5bc8e48d93f
WissemAouichi <wissem.aouichi@gmail.com>
2021-02-18 23:41:18 +0100

    added data to plotly graph

--- src/app/plotly/plotly graph/plotly-graph.component.ts
+++ src/app/plotly/plotly graph/plotly-graph.component.ts
@@ -8,5 +8,11 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
 export class PlotlyGraphComponent {
     @ViewChild("Graph", { static: true })
 private Graph: ElementRef; 
-}
+this.data = {
+    x: [ 1,2,3,4,5 ],
+    y: [ 10,20,30,40,50 ], //keeping the length same
+    name: 'type string, name of the trace',
+    type: 'scattergl', //this very important to activate WebGL
+    mode: 'line' //other properties can be found in the docs.
+  }
 }
