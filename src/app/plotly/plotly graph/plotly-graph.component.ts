6d46bcf46eee9a2a7050dc2096c2abe33c96c059
WissemAouichi <wissem.aouichi@gmail.com>
2021-02-18 23:42:28 +0100

    added plotly parameters

--- src/app/plotly/plotly graph/plotly-graph.component.ts
+++ src/app/plotly/plotly graph/plotly-graph.component.ts
@@ -15,4 +15,40 @@ this.data = {
     type: 'scattergl', //this very important to activate WebGL
     mode: 'line' //other properties can be found in the docs.
   }
+  this.Graph = Plotly.newPlot( 
+    this.Graph.nativeElement, //our viewchild element
+    this.data, //data provided
+    { //here starts the layout definition (keep in mind the commas)
+    autoexpand: "true",
+    autosize: "true",
+    width: window.innerWidth - 200, //we give initial width, so if the
+                                    //graph is rendered while hidden, it   
+                                    //takes the right shape
+    margin: {
+    autoexpand: "true",
+    margin: 0
+    },
+    offset: 0,
+    type: "scattergl",
+    title: name, //Title of the graph
+    hovermode: "closest",
+    xaxis: {
+    linecolor: "black",
+    linewidth: 2,
+    mirror: true,
+    title: "Time (s)",
+    automargin: true
+    },
+    yaxis: {
+    linecolor: "black",
+    linewidth: 2,
+    mirror: true,
+    automargin: true
+    title: 'Any other Unit'
+        }
+    },
+    { //this is where the configuration is defined
+    responsive: true, //important to keep graph responsive
+    scrollZoom: true
+    });
 }
