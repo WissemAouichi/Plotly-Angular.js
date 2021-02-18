267271681108d9b9dc5880d17293b3703e844f3a
WissemAouichi <wissem.aouichi@gmail.com>
2021-02-18 23:37:56 +0100

    added plotly graph component

---
+++ src/app/plotly/plotly graph/plotly-graph.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'plotly-graph',
+    templateUrl: 'plotly-graph.component.html'
+})
+
+export class PlotlyGraphComponent {
+    
+}
